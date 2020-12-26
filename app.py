d5b885153a55ce1b9f0bbe21506ba2e6c4498b28
euguroglu <e.uguroglu@yahoo.com>
2020-12-26 16:50:51 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
+class Member(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(50))
+
+class Orders(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    total = db.Column(db.Integer)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ readme.txt
@@ -0,0 +1,8 @@
+flask db init
+flask db migrate
+flask db upgrade
+
+command to check sqlite
+sqlite3 data.sqlite
+.tables
+.exit
