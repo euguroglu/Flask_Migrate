e8634aff8944034cac609e06225d07c620ef62e2
euguroglu <e.uguroglu@yahoo.com>
2020-12-26 17:31:26 +0300

    mysql

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ import os
 
 app = Flask(__name__)
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'xxxxqlhosting.netxxx8'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -15,6 +16,7 @@ migrate = Migrate(app,db)
 class Member(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(50))
+    subscribed = db.Column(db.Boolean)
 
 class Orders(db.Model):
     id = db.Column(db.Integer,primary_key=True)
